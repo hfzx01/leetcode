f1dcaff6f0d68eea865c2a6a31edeacf6771a060
hfzx01 <1799094080@qq.com>
2025-03-23 23:23:55 +0800

    刷题

--- 146.py
+++ 146.py
@@ -27,15 +27,56 @@ class Doublelist:
         self.size -= 1
 
     def deleteFirst(self):
+        if self.head == self.tail:
+            return None
+        first = self.head.next
+        self.delete(first)
+        return first
+
+    def size(self):
+        return self.size
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.map = {}
+        self.nodelist = Doublelist()
+        self.cap = capacity
 
     def get(self, key: int) -> int:
+        if key not in self.map:
+            return -1
+        self.makeRecently(key)
+        return self.map[key].val
 
     def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            self.deletekey(key)
+
+        if self.nodelist.size == self.cap:
+            self.removelastRecently()
+
+        self.addRecently(key, value)
+
+    def makeRecently(self, key):
+        x = self.map[key]
+        self.nodelist.delete(x)
+        self.nodelist.addatlast(x)
+
+    def addRecently(self, key, val):
+        x = Node(key, val)
+        self.map[key] = x
+        self.nodelist.addatlast(x)
+
+    def deletekey(self, key):
+        x = self.map[key]
+        self.nodelist.delete(x)
+        self.map.pop(key)
+
+    def removelastRecently(self):
+        x = self.nodelist.deleteFirst()
+        self.map.pop(x.key)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
